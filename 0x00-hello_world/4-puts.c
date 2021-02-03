2d2b26bc33fb255c21ffcfd3a38af3225dad86eb
AtsukiChan <mcfan227@gmail.com>
2021-02-03 17:19:56 +0000

    Betty approved! (for real this time)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/* main function prints the required text and returns 0 */
+/**
+* main - function prints the required text and returns 0
+* Return: 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
