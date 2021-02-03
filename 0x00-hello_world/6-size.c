da519622283614466f4bc0e6751fec5f33ada12d
AtsukiChan <mcfan227@gmail.com>
2021-02-03 18:57:25 +0000

    Removed variable definitions

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
-#include <sys/cdefs.h>
 /**
 * main - Defines all the data types, then prints their size using sizeof.
 * Return: 0
 */
 int main(void)
 {
-	char charType;
-	int intType;
-	long int longintType;
-	long long int longlongintType;
-	float floatType;
-
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of char: %zu byte\n", sizeof(charType));
-	printf("Size of long int: %zu byte\n", sizeof(longintType));
-	printf("Size of long long int: %zu byte\n", sizeof(longlongintType));
-
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(float));
+	printf("%lu\n", sizeof(long int));
+	printf("%lu\n", sizeof(long long int))
 	return (0);
 }
