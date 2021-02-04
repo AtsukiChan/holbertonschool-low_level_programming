47c5e28e5427b97e01ec1ab4d64420ff6dcf01bd
AtsukiChan <mcfan227@gmail.com>
2021-02-04 20:54:16 +0000

    Task 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - does the thing
+* Return: 0
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 100 ; a++)
+	{
+	putchar((a / 10) + '0');
+	putchar((a % 10) + '0');
+
+	if (a <= 98)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+		putchar('\n')
+		return (0)
+}
