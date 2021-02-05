34b40eb2a77b67c23e60480aeb5ce118f0da7458
AtsukiChan <mcfan227@gmail.com>
2021-02-05 03:05:39 +0000

    Task 13, fixed and working!

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,29 +7,21 @@ int main(void)
 {
 	int a;
 	int b;
-	int c;
-	int d;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 0; a <= 99 ; a++)
 	{
-		for (b = a + 1; b <= '9'; b++)
-		{	
-			for (c = b + 1; c <= '9'; c++)
-			{
-				for (d = c + 1; d <= '9'; d++)
-				{
-				putchar(a);
-				putchar(b);
-				putchar(' ');
-				putchar(c);
-				putchar(d);
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
 
-				if (a != '7')
-					{
-					putchar(',');
-					putchar(' ');
-					}
-				}
+			if (a <= 97)
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
