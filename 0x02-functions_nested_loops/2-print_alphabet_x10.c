95e2c432c09759d2179e26727dd2dc7c1a1424ce
AtsukiChan <mcfan227@gmail.com>
2021-02-09 16:20:04 +0000

    Test

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,17 +11,13 @@ void print_alphabet_x10(void)
 	char lowercaseLetter;
 	int numOfRows;
 
-	for (int numOfRows = 0; numOfRows <= 9; numOfRows++)
+	for (numOfRows = 0; numOfRows <= 9; numOfRows++)
 	{
-
 		for (lowercaseLetter = 97; lowercaseLetter <= 122; lowercaseLetter++)
 		{
-
 			_putchar(lowercaseLetter);
-
 		}
-	_putchar('\n');
-
+		_putchar('\n');
 	}
 }
 
