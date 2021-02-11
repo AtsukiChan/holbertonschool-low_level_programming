429c42850fef6501f4a4ced7ad9c859127354721
AtsukiChan <mcfan227@gmail.com>
2021-02-11 17:52:05 +0000

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_line - draws a line
+ * @n: line length
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (line = 0; line < num; line++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
