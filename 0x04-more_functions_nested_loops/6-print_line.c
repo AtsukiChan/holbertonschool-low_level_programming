468c5e5670d2c97707c62edfe86ae73e51fa0c83
AtsukiChan <mcfan227@gmail.com>
2021-02-11 18:29:13 +0000

    Not working but pushing it anyways

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "holberton.h"
 
 /**
@@ -5,6 +6,16 @@
  * @n: line length
  */
 
+void print_line(int n);
+
+int main(void)
+{
+	print_line(0);
+	print_line(2);
+	print_line(10);
+	print_line(-4);
+	return (0);
+}
 void print_line(int n)
 {
 	int i;
@@ -12,7 +23,7 @@ void print_line(int n)
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
-			_putchar('_');
+			putchar('_');
 	}
-	_putchar('\n');
+	putchar('\n');
 }
