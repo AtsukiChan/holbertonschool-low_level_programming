07d1916a39692b4aa9e92a3546147d54cdb50642
AtsukiChan <mcfan227@gmail.com>
2021-02-12 03:35:09 +0000

    Fixed operations, added comments

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Fizz-Buzz test
+ * Return: 0
+ */
+
 int main(void)
 {
 	int n;
 
 	for (n = 1; n < 100; n++)
 	{
-		if ((n % 3 == 0) %% (n % 5 != 0))
+		if ((n % 3 == 0) && (n % 5 != 0))
 		{
 			printf("Fizz ");
 		}
